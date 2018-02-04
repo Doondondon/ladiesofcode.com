3ab4a682ec244bf830c0702bfe7906e4ca718485
angiemaguire <angelamaguire2@gmail.com>
2018-02-04 18:34:41 +0700

    formatted events data

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 const fetchJsonP = require("fetch-jsonp");
+const moment = require('moment');
 
 const init = function() {
   const options = {
@@ -32,17 +33,32 @@ function append(parent, el) {
   return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
+function showDate(time) {
+  return moment(time).format("MMMM Do, YYYY");
+}
+  
+function mapLink(venue) {
+  const { name, lat, lon, city } = venue;
+  const safeName = encodeURIComponent(name)
+  return `http://www.google.com/maps/place/${safeName},${city}/${lat},${lon}`
+  }
+
 const renderEvents = data => {
   const ul = document.getElementById("events-upcoming");
   let events = data.results;
   return events.map(event => {
     let li = createNode("li"),
+      div = createNode('div'),
       span = createNode("span"),
       a = createNode("a");
     a.href = event.event_url;
     a.innerHTML = `${event.name}`;
-    append(span, a);
-    append(li, span);
+    li.className = "col-4 p2 border-box border m4";
+    span.innerHTML = `<h3>${showDate(event.time)}</h2><p>Venue: ${event.venue ? `<a href="${mapLink(event.venue)}">${event.venue.name}</a>` : 'TBC'}</p>`;
+
+    append(div, span)
+    append(div, a);
+    append(li, div);
     append(ul, li);
   });
 };
